7bb6af9c55b214924291c114c078d750ca38676a
Eduardo Moraes <eduardomoraesmail@gmail.com>
2026-05-27 12:05:26 -0300

    added logic to conect server to rabbitmq

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	const connectionString = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Printf("Failed to dial: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Println("Sucessfully connected")
+
+	channel, err := conn.Channel() 
+	if err != nil {
+		fmt.Printf("Failed to create channel: %v\n", err)
+		os.Exit(1)
+	}
+
+	pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+
+
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Stopping Peril server...")
 }
