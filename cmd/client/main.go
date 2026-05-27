0b8dd51574d449b99636e33d1ada39ca40840468
Eduardo Moraes <eduardomoraesmail@gmail.com>
2026-05-27 14:00:48 -0300

    added pubsub logic to client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	const connectionString = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Printf("Failed to dial: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Println("Sucessfully connected")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Printf("Failed to get username: %v\n", err)
+		os.Exit(1)
+	}
+
+	_, _, err = pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		fmt.Sprintf("%s.%s", routing.PauseKey, username),
+		routing.PauseKey, 
+		pubsub.SimpleQueueType(1),
+	)
+	if err != nil {
+		fmt.Printf("Failed to declare and bind: %v\n", err)
+		os.Exit(1)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Stopping Peril client...")
+
 }
---
+++ internal/pubsub/pubsub_config.go
@@ -0,0 +1,50 @@
+package pubsub
+
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type SimpleQueueType int
+
+const (
+	durable SimpleQueueType = iota
+	transient
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType, // SimpleQueueType is an "enum" type I made to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+	channel, err := conn.Channel() 
+	if err != nil {
+		fmt.Printf("Failed to create channel: %v\n", err)
+		return nil, amqp.Queue{}, err
+	}
+
+	isQueueDurable := false
+	autoDelete := false
+	exclusive := false
+
+	switch queueType {
+	case durable:
+		isQueueDurable = true
+	case transient:
+		autoDelete = true
+		exclusive = true
+	}
+
+	queue, err := channel.QueueDeclare(queueName, isQueueDurable, autoDelete, exclusive, false, nil)
+	if err != nil {
+		fmt.Printf("Failed to create queue: %v\n", err)
+		return nil, amqp.Queue{}, err
+	}
+	
+	channel.QueueBind(queue.Name, key, exchange, false, nil)
+
+	return channel, queue, nil
+}
