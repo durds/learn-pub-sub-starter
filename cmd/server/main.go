b3a4b130ed90a24e546363582c9a3bc280c2a327
Eduardo Moraes <eduardomoraesmail@gmail.com>
2026-05-27 15:43:02 -0300

    added game logic to the server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+func handleSignals(cancel context.CancelFunc) {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	fmt.Println("Setting up signals handler")
+	<-signalChan
+	fmt.Println("Ctrl + C received")
+	cancel()
+}
+
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	gamelogic.PrintServerHelp()
 
 	const connectionString = "amqp://guest:guest@localhost:5672/"
 	conn, err := amqp.Dial(connectionString)
@@ -23,21 +36,46 @@ func main() {
 
 	fmt.Println("Sucessfully connected")
 
-	channel, err := conn.Channel() 
+	channel, err := conn.Channel()
 	if err != nil {
 		fmt.Printf("Failed to create channel: %v\n", err)
 		os.Exit(1)
 	}
 
-	pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
-		IsPaused: true,
-	})
+	ctx, cancel := context.WithCancel(context.Background())
 
+	go handleSignals(cancel)
 
+	for {
+		select {
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+		case <-ctx.Done():
+			fmt.Println("Stopping Peril server...")
+			return
+		default:
+			words := gamelogic.GetInput()
+			if len(words) == 0 {
+				continue
+			}
 
-	fmt.Println("Stopping Peril server...")
+			if len(words) > 0 && words[0] == "pause" {
+				fmt.Println("Sending a pause message")
+				pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+					IsPaused: true,
+				})
+			} else if len(words) > 0 && words[0] == "resume" {
+				fmt.Println("Sending a resume message")
+				pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+					IsPaused: false,
+				})
+			} else if len(words) > 0 && words[0] == "quit" {
+				fmt.Println("Quitting")
+				return
+			} else {
+				fmt.Printf("Invalid command: %s\n", words[0])
+			}
+		}
+
+	}
+
 }
